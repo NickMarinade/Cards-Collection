f92d8d50674a92f63c10e03aa41e6d1487ce97a3
NickMarinade <59340748+NickMarinade@users.noreply.github.com>
2023-01-10 12:31:09 +0100

    colorsFix
    trying new approach with colors

--- create/create.js
+++ create/create.js
@@ -15,6 +15,15 @@ const randomName = (names) => {
 };
 //console.log(randomName(names))
 
+// var colors = [];
+// while (colors.length < 100) {
+//     do {
+//         var color = Math.floor((Math.random()*1000000)+1);
+//     } while (colors.indexOf(color) >= 0);
+//     colors.push("#" + ("000000" + color.toString(16)).slice(-6));
+// }
+// console.log(colors);
+
 
  
 
