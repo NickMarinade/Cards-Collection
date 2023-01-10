8ce7d258aab3291b8d14e9fb95f9a9b55b70de5c
NickMarinade <59340748+NickMarinade@users.noreply.github.com>
2023-01-10 15:55:05 +0100

    domSelectors
    fix for color logic

--- domSelectors/domSelectors.js
+++ domSelectors/domSelectors.js
@@ -10,19 +10,34 @@ document.querySelector('img.important').style.display = 'none';
 console.log(document.querySelectorAll('p'))
 
 //Give each of the paragraph a random text color (different for each one) UNLESS it has a class then leave it as it is.
-const randomColor = Math.floor(Math.random()*16777215).toString(16);
+let alreadyDone = [];
+
+let colors = [];
+while (colors.length < 500) {
+    do {
+        var color = Math.floor((Math.random()*16777215));
+    } while (colors.indexOf(color) >= 0);
+    colors.push("#" + (color.toString(16)).slice(-6));
+}
+//console.log(colors);
+
+
+
+
+const randomName = (names) => {
+  if (alreadyDone.length === 0) {
+    for (let i = 0; i < names.length; i++) alreadyDone.push(i);
+  }
+  let randomNameIndex = Math.floor(Math.random() * alreadyDone.length);
+  let indexOfItemInMyNames = alreadyDone[randomNameIndex];
+  alreadyDone.splice(randomNameIndex, 1);
+  return names[indexOfItemInMyNames];
+};
 
 for (let i = 1; i < document.querySelectorAll('p').length; i++) {
     
-    document.querySelectorAll('p')[i].style.color = '#' + randomColor;
+    document.querySelectorAll('p')[i].style.color =  randomName(colors);
   }
 
-
-  //const collection = document.querySelectorAll('p');
-  //console.log(collection);
-
-  //collection[1].style.color = '#' + randomColor;
-  //collection[2].style.color = '#' + randomColor;
-  //collection[3].style.color = '#' + randomColor;
 
 
