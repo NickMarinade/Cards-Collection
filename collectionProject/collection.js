d84d992bc6a02846871eb3dbfaeb8422fd0e358b
NickMarinade <59340748+NickMarinade@users.noreply.github.com>
2023-01-11 12:05:38 +0100

    collection
    collection of songs done

---
+++ collectionProject/collection.js
@@ -0,0 +1,99 @@
+const collection = [
+    {
+        artist: ['Bring Me The Horizon, BABYMETAL'],
+        album: 'POST HUMAN: SURVIVAL HORROR',
+        song: 'Kingslayer',
+        genre: 'Post-Hardcore',
+        releaseYear: '2020',   
+    },
+
+    {
+        artist: ['Worakls, Rusanda Panfili'],
+        album: 'Single',
+        song: 'Storm',
+        genre: 'Deep House',
+        releaseYear: '2020',   
+    },
+
+    {
+        artist: ['Bring Me The Horizon, BABYMETAL'],
+        album: 'POST HUMAN: SURVIVAL HORROR',
+        song: 'Kingslayer',
+        genre: 'Post-Hardcore',
+        releaseYear: '2020',   
+    },
+
+    {
+        artist: 'MIKA',
+        album: 'Single',
+        song: 'Yo Yo',
+        genre: 'POP',
+        releaseYear: '2022',   
+    },
+
+    {
+    artist: 'Arctic Monkeys',
+    album: 'Favourite Worst Nightmare',
+    song: 'Brainstorm',
+    genre: 'Alternative Rock',
+    releaseYear: '2007',   
+},
+
+{
+    artist: 'SiM',
+    album: 'Single',
+    song: 'The Rumbling',
+    genre: 'J-Rock',
+    releaseYear: '2022',   
+},
+
+{
+    artist: 'Mick Gordon',
+    album: 'Doom Original Game Soudtrack',
+    song: 'Rip & Tear',
+    genre: 'Speed Metal',
+    releaseYear: '2016',   
+},
+
+{
+    artist: 'KASAI HARCORES',
+    album: 'KASAI HARCORES ALL STAR',
+    song: 'CYCLE HIT',
+    genre: 'Dance',
+    releaseYear: '2017',   
+},
+
+{
+    artist: 'Avenged Sevenfold',
+    album: 'Avenged Sevenfold',
+    song: 'Scream',
+    genre: 'Rock',
+    releaseYear: '2020',   
+},
+
+{
+    artist: 'AC/DC',
+    album: 'Back in Black',
+    song: 'Shoot to Thrill',
+    genre: 'Rock',
+    releaseYear: '1980',   
+},
+
+{
+    artist: 'Woodkid',
+    album: 'Arcane Soundtrack',
+    song: 'Guns for Hire',
+    genre: 'Neofolk',
+    releaseYear: 2021,   
+},
+]
+
+
+
+
+
+
+
+
+
+
