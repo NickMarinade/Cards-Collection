9e4a581a0a2ebb87df9f4e107b3a5de5b43dc7c1
NickMarinade <59340748+NickMarinade@users.noreply.github.com>
2023-01-12 13:33:53 +0100

    make 2 lists of cards

--- collectionProject/collection.js
+++ collectionProject/collection.js
@@ -84,12 +84,16 @@ const list2 =  [
     },
 ]
 
+let section = document.createElement('section');
+section.classList.add("card-list");
+let section2 = document.createElement('section');
+section2.classList.add("card-list");
+section2.setAttribute('id', 'section2');
+let body = document.querySelector('body');
+body.append(section, section2);
+console.log(body);
+
 function addList1() {
-    
-    let body = document.querySelector('body');
-    let section = document.querySelector('section');
-    section.classList.add("card-list");
-    body.append(section);
 
     let article = document.createElement('article');
     article.classList.add('card')
@@ -146,7 +150,67 @@ function addList1() {
 }
 
 
+function addList2() {
+
+    let article = document.createElement('article');
+    article.classList.add('card')
+    section2.append(article);
+
+    let header = document.createElement('header');
+    header.classList.add('card-header')
+    article.append(header);
+    let paragraphHeader = document.createElement('p');
+    let h2Header = document.createElement('h2');
+    header.append(paragraphHeader, h2Header);
+    let releaseContent = document.createTextNode(list2[i].releaseYear);
+    let albumContent = document.createTextNode(list2[i].album);
+    paragraphHeader.append(releaseContent);
+    h2Header.append(albumContent);
+
+    let div1 = document.createElement('div');
+    div1.classList.add('card-main');
+    article.append(div1);
+    let imgLink = document.createElement('a');
+    imgLink.classList.add('main-img');
+    imgLink.href = '#';
+    div1.append(imgLink);
+    let imageSpotify = document.createElement('img');
+    imageSpotify.src = 'https://e1.pngegg.com/pngimages/479/641/png-clipart-spotify-macos-style-spotify-logo-thumbnail.png'
+    imgLink.append(imageSpotify);
+    // let imgLink2 = document.createElement('a');
+    // imgLink2.classList.add('main-img');
+    // imgLink2.setAttribute('id', 'secondImg')
+    // imgLink2.href = '#';
+    // div1.append(imgLink2);
+    // let imageSong = document.createElement('img');
+    // imgLink2.append(imageSong);
+
+    let div2 = document.createElement('div');
+    div2.classList.add('main-name');
+    article.append(div2);
+    let paragraphName = document.createElement('p');
+    let h2Name = document.createElement('h2');
+    div2.append(h2Name, paragraphName);
+    let songContent = document.createTextNode(list2[i].song);
+    let artistContent = document.createTextNode(list2[i].artist);
+    paragraphName.append(artistContent);
+    h2Name.append(songContent);
+
+    let div3 = document.createElement('div');
+    div3.classList.add('genre');
+    article.append(div3);
+    let paragraphGenre = document.createElement('p');
+    div3.append(paragraphGenre);
+    let genreContent = document.createTextNode(list2[i].genre);
+    paragraphGenre.append(genreContent);
+
+}
+
+
+
+
 for (var i = 0; i < list1.length; i++) addList1(i);
+for (var i = 0; i < list1.length; i++) addList2(i);
 console.log(document.querySelector('body'))
 
 
