38abbfbc4b5cf9d69af885020a90f78815280277
NickMarinade <59340748+NickMarinade@users.noreply.github.com>
2023-01-13 15:07:49 +0100

    Move the <h2> between sections

--- navigate/navigate.js
+++ navigate/navigate.js
@@ -4,7 +4,32 @@ const parentSection = document.querySelector('ol');
 const firstChild = parentSection.children[0];
 const lastChild = parentSection.children[4];
 parentSection.insertBefore(lastChild,firstChild);
-console.log(parentSection);
+//console.log(parentSection);
+
+//Move the <h2> of the third section in the second one and vice-versa
+
+const body = document.querySelector('body');
+const main = body.children[1];
+//console.log(main);
+const thirdSection = main.children[1];
+//console.log(thirdSection);
+const secondSection = main.children[2];
+const h2Third = thirdSection.children[0];
+//console.log(h2Third);
+const h2Second = secondSection.children[0].children[0];
+//console.log(h2Second);
+secondSection.append(h2Third);
+//console.log(secondSection);
+thirdSection.append(h2Second);
+
+
+
+
+
+
+
+
+
 
 
 
