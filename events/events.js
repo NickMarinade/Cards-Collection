60867eb764513bcbe449e4877b5260e9977d76a0
NickMarinade <59340748+NickMarinade@users.noreply.github.com>
2023-01-16 12:02:09 +0100

    final logic for display new squares

--- events/events.js
+++ events/events.js
@@ -1,23 +1,37 @@
+let wrapperSection = document.querySelector('.displayedsquare-wrapper');
+
+console.log(wrapperSection)
+
+const actionSquare = document.querySelector('.actionsquare');
+
 const _initTime = Date.now()
 
 const getElapsedTime = () => {
   return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's'
 }
-
 const clickOnSquare = (e) => {
   //debugger;
-  const wrapper = document.getElementsByClassName('actionsquare-wrapper');
-  const newDiv = document.createElement('div');
-  newDiv.classList.add('displayedsquare');
-  
-  const actionSquare = document.getElementsByClassName('actionsquare');
   
   if (e.target.classList[1] === 'green') {
-    console.log('green');
+    let newGreen = document.createElement('div');
+    newGreen.classList.add('green');
+    newGreen.classList.add('displayedsquare');
+    wrapperSection.append(newGreen);
+    //console.log('green');
   } else if(e.target.classList[1] === 'violet') {
-    console.log('violet')
-  } else{
-    console.log('orange')
+    let newViolet = document.createElement('div');
+    newViolet.classList.add('violet');
+    newViolet.classList.add('displayedsquare');
+    wrapperSection.append(newViolet);
+    //console.log('violet')
+  } else if (e.target.classList[1] === 'orange') {
+    let newOrange = document.createElement('div');
+    newOrange.classList.add('orange');
+    newOrange.classList.add('displayedsquare');
+    wrapperSection.append(newOrange);
+    //console.log('orange')
+  } else {
+    console.error('error');
   }
   // console.log(actionSquare);
   // console.log(e.target.classList[1])
@@ -30,6 +44,8 @@ const actionSquares = document.querySelectorAll('.actionsquare')
 for (let actionSquare of actionSquares) {
   actionSquare.addEventListener('click', clickOnSquare)
 }
+
+
 
 // Everytime the user clicks on one of the action squares
 //  - Create a new <div> with a class .displayedsquare and the corresponding clicked color in the div above (.displayedsquare-wrapper)
