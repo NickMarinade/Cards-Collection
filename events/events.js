d5439620b5e0ee5fa708a2da77819375902cdc55
NickMarinade <59340748+NickMarinade@users.noreply.github.com>
2023-01-16 13:32:46 +0100

    complete log output

--- events/events.js
+++ events/events.js
@@ -1,6 +1,5 @@
 let wrapperSection = document.querySelector('.displayedsquare-wrapper');
-
-console.log(wrapperSection)
+let logUl = document.querySelector('ul');
 
 const actionSquare = document.querySelector('.actionsquare');
 
@@ -9,33 +8,38 @@ const _initTime = Date.now()
 const getElapsedTime = () => {
   return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's'
 }
+
 const clickOnSquare = (e) => {
   //debugger;
   
   if (e.target.classList[1] === 'green') {
     let newGreen = document.createElement('div');
+    let greenLog = document.createElement('li');
+    greenLog.append(getElapsedTime(), ', Green');
+    logUl.append(greenLog);
     newGreen.classList.add('green');
     newGreen.classList.add('displayedsquare');
     wrapperSection.append(newGreen);
-    //console.log('green');
   } else if(e.target.classList[1] === 'violet') {
     let newViolet = document.createElement('div');
+    let violetLog = document.createElement('li');
+    violetLog.append(getElapsedTime(), ', Violet');
+    logUl.append(violetLog);
     newViolet.classList.add('violet');
     newViolet.classList.add('displayedsquare');
     wrapperSection.append(newViolet);
-    //console.log('violet')
   } else if (e.target.classList[1] === 'orange') {
     let newOrange = document.createElement('div');
+    let orangeLog = document.createElement('li');
+    orangeLog.append(getElapsedTime(), ', Orange');
+    logUl.append(orangeLog);
     newOrange.classList.add('orange');
     newOrange.classList.add('displayedsquare');
     wrapperSection.append(newOrange);
-    //console.log('orange')
   } else {
     console.error('error');
   }
-  // console.log(actionSquare);
-  // console.log(e.target.classList[1])
-  // console.log(getElapsedTime())
+
 }
 
 
@@ -47,9 +51,6 @@ for (let actionSquare of actionSquares) {
 
 
 
-// Everytime the user clicks on one of the action squares
-//  - Create a new <div> with a class .displayedsquare and the corresponding clicked color in the div above (.displayedsquare-wrapper)
-//  - Create a new <li> in the log below to state when the action was done
 
 
 
